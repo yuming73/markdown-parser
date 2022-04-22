01c5a269898187c07c095aff8d31ff9440b71257
yuming73 <103291654+yuming73@users.noreply.github.com>
2022-04-22 16:21:45 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -40,5 +40,6 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+        System.out.println(".");
     }
 }
