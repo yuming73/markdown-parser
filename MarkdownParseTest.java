94102c034074a94e587c337efc636c362d42ca84
yuming73 <103291654+yuming73@users.noreply.github.com>
2022-04-22 16:47:12 -0700

    Update MarkdownParseTest.java
    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
    java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore  MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,14 @@
+//imports the packages needed to use JUnit
 import static org.junit.Assert.*;
 import org.junit.*;
 
+//defines the class
 public class MarkdownParseTest {
+    //defines the code below to be a test
     @Test
+    //defines the method
     public void addition() {
+        //testing that the actual result from 1+1 is equal to the expected output of 2
         assertEquals(2, 1+1);
     }
 }
