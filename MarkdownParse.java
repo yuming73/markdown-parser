438f5310c722c97178ae2af0b16a66afe6a040d5
yuming73 <103291654+yuming73@users.noreply.github.com>
2022-04-22 17:27:30 -0700

    Update MarkdownParse.java
    Errors: IndexOutOfBounds, java needs more heap space

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -34,12 +34,10 @@ public class MarkdownParse {
         return toReturn;
     }
 
-
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
-        System.out.println(".");
     }
 }
