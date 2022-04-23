73b6f8dc79f247dfcdc74453250a4c39e2698247
yuming73 <103291654+yuming73@users.noreply.github.com>
2022-04-22 17:08:57 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,11 @@
 //imports the packages needed to use JUnit
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.*;
 
 //defines the class
@@ -10,5 +16,12 @@ public class MarkdownParseTest {
     public void addition() {
         //testing that the actual result from 1+1 is equal to the expected output of 2
         assertEquals(2, 1+1);
+    }
+    @Test
+    public void testGetLinks() throws IOException {
+        List testList = List.of("https://something.com", "some-thing.html");
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        assertEquals(testList, MarkdownParse.getLinks(content));
     }
 }
