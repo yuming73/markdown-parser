663cc837b291cb4a3b64b9ce51ca4ee02f1905b0
yuming73 <103291654+yuming73@users.noreply.github.com>
2022-05-22 16:17:45 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,23 +17,12 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            
-            //checks if the links contains any parentheses
-            int index = markdown.indexOf("(", closeParen-1);
-            if(index == closeParen-1) {
-                closeParen = markdown.indexOf(")", closeParen+1);
-            }
 
             //checks if the index is out of bounds
             if(openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1){
                 break;
             }
             
-            //checks if the brackets contains backticks
-            if(markdown.substring(closeBracket, closeBracket + 2).contains("`")) {
-                closeBracket += 2;
-            }
-
             //checks if the parentheses contain spaces
             if(markdown.substring(openParen, closeParen).contains(" ")) {
                 currentIndex = closeParen + 1;
@@ -48,9 +37,24 @@ public class MarkdownParse {
                 }
             }
 
+            //checks if there is brackets between the openParen and closeBracket
+            if(markdown.substring(closeBracket + 1, openParen).contains("]")) {
+                closeBracket = markdown.indexOf("]", closeBracket + 1);
+            }
+
+            //checks if the link contains any parentheses
+            if(markdown.substring(openParen + 1, closeParen).contains("(")) {
+                closeParen = markdown.indexOf(")", closeParen + 1);
+                if(closeParen + 1 < markdown.length()) {
+                    if(markdown.substring(closeParen + 1, closeParen + 2).equals(")")) {
+                        closeParen += 1;
+                    }
+                }
+            } 
+
             //trims the link before adding to the list (remove any spaces)
             if(openParen - closeBracket == 1) {
-                String link = markdown.substring(openParen + 1, closeParen);
+                String link = markdown.substring(openParen+1, closeParen);
                 toReturn.add(link.trim());
             }
             currentIndex = closeParen + 1;
